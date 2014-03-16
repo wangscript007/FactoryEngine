8a9f06401410b6cd0a3dc9451ab9908ddbc79667
Edvinas Sarkus <edvinas.sarkus@gmail.com>
2014-03-16 11:12:55 +0200

    Face traversal direction check.

--- Model/FaceTraversal.cpp
+++ Model/FaceTraversal.cpp
@@ -47,8 +47,10 @@ bool FaceTraversal::Find(ftr::Edge *startEdge)
 {
     std::cout << "Checking " << startEdge->Name() << std::endl;
     PointNode* originNode = startEdge->originNode();
+    int index = 0;
     for(PointNode::Iterator i = originNode->Begin(); i != originNode->End(); ++i)
     {
+        index++;
         ftr::Edge* iEdge = *i;
         if (iEdge != startEdge->twin() && iEdge->IsFree())
         {
@@ -57,25 +59,24 @@ bool FaceTraversal::Find(ftr::Edge *startEdge)
                 mEdgesVector.push_back(iEdge);
                 return true;
             }
-            else {
-                if (iEdge->next())
-                {
-                    bool samePlane = true;
-                    if (mEdgesVector.size() > 1) {
-                        // samePlane check if plane would be same as first two vector elements
-                    }
-                    if (samePlane)
-                    {
-                        mEdgesVector.push_back(iEdge);
-                        
-                        if (Find(iEdge->next()))
-                        {
-                            return true;
-                        }
-                        mEdgesVector.pop_back();
-                    }
+            else if (iEdge->next()) {
+                bool samePlane = true;
+                bool isCW = true;
+                if (mEdgesVector.size() > 0) {
+                    // samePlane check if plane would be same as first two vector elements
+                    isCW = iEdge->IsCCWCountingFrom(*mEdgesVector[index-1]);
+//                    assert(isCW);
                 }
-                
+                if (samePlane && isCW)
+                {
+                    mEdgesVector.push_back(iEdge);
+                    
+                    if (Find(iEdge->next()))
+                    {
+                        return true;
+                    }
+                    mEdgesVector.pop_back();
+                }
             }
         }
     }
