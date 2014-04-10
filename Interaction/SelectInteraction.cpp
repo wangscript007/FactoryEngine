ecb9ab552d695a4180f28d439d1649b6ac5caf11
Edvinas Sarkus <edvinas.sarkus@gmail.com>
2014-04-10 22:07:10 +0300

    [FEATURE] Selection Tool: SelectInteraction

---
+++ Interaction/SelectInteraction.cpp
@@ -0,0 +1,49 @@
+
+#include <Model/PointNode.h>
+#include <Processing/ModelEditor.h>
+#include <Interaction/SelectInteraction.h>
+
+namespace ftr {
+    
+SelectInteraction::SelectInteraction(ModelEditor& modelEditor, const Viewport& viewport)
+:mModelEditor(modelEditor)
+,mActive(false)
+{
+}
+
+SelectInteraction::~SelectInteraction()
+{
+ 
+}
+
+#pragma mark Instance
+
+void SelectInteraction::Render(Layer& layer)
+{
+    Node::Render(layer);
+}
+
+void SelectInteraction::Begin()
+{
+    if (!mActive) {
+        mActive = true;
+        
+        
+        mEnd = glm::vec3();
+        mStart = glm::vec3();
+        
+    }
+}
+
+void SelectInteraction::Step()
+{
+}
+
+void SelectInteraction::Finish()
+{
+    mActive = false;
+}
+
+
+
+}
---
+++ Interaction/SelectInteraction.h
@@ -0,0 +1,34 @@
+
+#pragma once
+
+#include <Model/Node.h>
+
+namespace ftr {
+    
+class ModelEditor;
+class PointNode;
+
+class SelectInteraction : public Node
+{
+public:
+    SelectInteraction(ModelEditor& modelEditor, const Viewport& viewport);
+    virtual ~SelectInteraction();
+    void Render(Layer& layer);
+    
+    void Begin();
+    void Step();
+    void Finish();
+    
+    const glm::vec3& start() const { return mStart; }
+    const glm::vec3& end() const { return mEnd; }
+    
+private:
+    glm::vec3 mStart;
+    glm::vec3 mEnd;
+    bool mActive;
+    
+    ModelEditor& mModelEditor;
+};
+    
+}
+
